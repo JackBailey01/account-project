54f2916b3eece18feaf4202ec00e10c910fe7faf
Jack Bailey <jackbailey261@gmail.com>
2018-08-23 11:57:07 +0100

    final

--- src/main/java/com/qa/accapp/Service.java
+++ src/main/java/com/qa/accapp/Service.java
@@ -1,10 +1,6 @@
 package com.qa.accapp;
 
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Service {
