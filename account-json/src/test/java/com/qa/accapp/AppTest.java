87d71572efd72a9ae5254a7877001c108289fbb6
JackBailey <JackBailey261@gmail.com>
2018-08-24 09:47:18 +0100

    red

--- account-json/src/test/java/com/qa/accapp/AppTest.java
+++ account-json/src/test/java/com/qa/accapp/AppTest.java
@@ -1,20 +1,17 @@
 package com.qa.accapp;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+  
     @Test
-    public void shouldAnswerWithTrue()
+    public void nameCount()
     {
-        assertTrue( true );
+        Service service = new Service();
+        int num = service.count("Jack");
+        assertEquals(2, num);
     }
 }
