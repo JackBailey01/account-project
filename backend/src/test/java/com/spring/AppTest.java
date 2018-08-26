3b9cde8fb2a30250c8c0a7ac6203375834042a53
Jack Bailey <jackbailey261@gmail.com>
2018-08-26 12:35:05 +0100

    TDD?

--- backend/src/test/java/com/spring/AppTest.java
+++ backend/src/test/java/com/spring/AppTest.java
@@ -1,7 +1,8 @@
 package com.spring;
 
-import static org.junit.Assert.assertTrue;
-
+import com.spring.model.Account;
+import com.spring.levels.Database;
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -9,8 +10,23 @@ public class AppTest
 {
 
     @Test
-    public void Database.add("Jack")
-    {
+    public void addandview(){
+        Database database = new Database();
+        Account account = new Account(0000,"Jack","Bailey");
+        database.add(account);
+        Account jack = database.find("Jack");
+        Assert.assertEquals("Jack", jack.getFirstName());
+        Assert.assertEquals("Bailey", jack.getLastName());
+        Assert.assertEquals("0", jack.getAccNo());
 
+    }
+    @Test
+    public void delete(){
+        Database database = new Database();
+        Account account = new Account(0000,"Jack","Bailey");
+        database.add(account);
+        database.delete("Jack");
+        Account jack = database.find("Jack");
+        Assert.assertEquals(null, jack.getFirstName());
     }
 }
