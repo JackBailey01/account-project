cf3b9a95fa24568fa56150dda9daaf2476466358
unknown <jackbailey261@gmail.com>
2018-08-29 12:46:59 +0100

    we

--- backend/src/main/java/com/spring/model/AccountRepositoryCustom.java
+++ backend/src/main/java/com/spring/model/AccountRepositoryCustom.java
@@ -3,9 +3,11 @@ package com.spring.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 
 @Repository
 public interface AccountRepositoryCustom {
     Account getById(Integer id);
+    List<Account> getAllAccounts();
 }
--- backend/src/main/java/com/spring/model/AccountRepositoryImpl.java
+++ backend/src/main/java/com/spring/model/AccountRepositoryImpl.java
@@ -1,21 +1,31 @@
 package com.spring.model;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
 
 
 @Repository
+@Transactional(SUPPORTS)
 public class AccountRepositoryImpl implements AccountRepositoryCustom {
     @PersistenceContext
     EntityManager entityManager;
 
     @Override
     public Account getById(Integer accNo){
-        Account account = entityManager.find(Account.class, accNo);
-        return account;
+        return entityManager.find(Account.class, accNo);
+    }
+
+    @Override
+    public List<Account> getAllAccounts(){
+        TypedQuery query = entityManager.createQuery("select A from accounts",Account.class);
+        return query.getResultList();
+
     }
 
 }
